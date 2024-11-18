b98a78ebf5b143471d575517ef639dd17142c1fe
GioZerini <123542650+GioZerini@users.noreply.github.com>
2024-11-18 15:41:26 +0100

    Update __init__.py

--- acds/benchmarks/__init__.py
+++ acds/benchmarks/__init__.py
@@ -1,5 +1,8 @@
 from .adiac.getter import get_adiac_data
 from .mackey_glass import get_mackey_glass, get_mackey_glass_windows
 from .mnist import get_mnist_data
+from .mallat import get_mallat_data
+from .trace import get_trace_data
+from .libras import get_libras_data
 
-__all__ = ["get_adiac_data", "get_mackey_glass", "get_mnist_data", "get_mackey_glass_windows"]
+__all__ = ["get_adiac_data", "get_mackey_glass", "get_mnist_data", "get_mackey_glass_windows", "get_mallat_data", "get_trace_data", "get_libras_data"]

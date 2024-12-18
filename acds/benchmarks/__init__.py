78c750b9ea8457bac004f0b8d94c478d56254cf2
GioZerini <g.zerini1@studenti.unipi.it>
2024-12-18 15:12:12 +0100

    Fixed __init__.py

--- acds/benchmarks/__init__.py
+++ acds/benchmarks/__init__.py
@@ -4,5 +4,6 @@ from .mnist import get_mnist_data
 from .mallat import get_mallat_data
 from .trace import get_trace_data
 from .libras import get_libras_data
+from .cifar10 import get_cifar10_data
 
-__all__ = ["get_adiac_data", "get_mackey_glass", "get_mnist_data", "get_mackey_glass_windows", "get_mallat_data", "get_trace_data", "get_libras_data"]
+__all__ = ["get_adiac_data", "get_mackey_glass", "get_mnist_data", "get_mackey_glass_windows", "get_mallat_data", "get_trace_data", "get_libras_data", "get_cifar10_data"]
